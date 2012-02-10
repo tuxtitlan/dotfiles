5104552f274e37eb2ff56f8eebf39abbb2e392fc
George Song <george@55minutes.com>
2012-02-10 14:11:14 -0800

    Let's process template scripts!
    --HG--
    rename : scripts/sleep.scpt => scripts/sleep.scpt.template

--- install_scripts.py
+++ install_scripts.py
@@ -4,24 +4,55 @@ import argparse
 import glob
 import os
 import subprocess
+import string
+import tempfile
+import codecs
 
 __VERSION__ = '1.0'
 
 
 # General utils
 ###############
+class Template(string.Template):
+    delimiter = '@'
+
+
 def call(cmd):
     return subprocess.call(cmd, shell=True)
+
+
+def open(filename, mode=None, *args, **kwargs):
+    if not mode:
+        mode = 'r'
+    return codecs.open(filename, mode, encoding='utf-8', *args, **kwargs)
 
 
 # Install files
 ###############
 def install_files(args):
-    dstdir = os.path.join(MY_PATH, 'scripts')
+    dstdir = args.directory
+    srcdir = os.path.join(MY_PATH, 'script')
     prepare(dstdir)
-    os.chdir(dstdir)
-    for f in glob.iglob('*.scpt'):
-        dst = os.path.join(dstdir, f)
+    os.chdir(srcdir)
+    for f in glob.iglob('*'):
+        dst = os.path.join(dstdir, f.replace('.template', ''))
+        process_file(os.path.join(srcdir, f), dst, args)
+
+
+def process_file(f, dst, args):
+    if f.endswith('.template'):
+        tmp = tempfile.mkstemp()[1]
+        t = Template(open(f, 'rU').read())
+        with open(tmp, 'w+') as of:
+            while True:
+                try:
+                    of.write(t.substitute(args.__dict__))
+                    break
+                except KeyError as e:
+                    setattr(args, e.message, raw_input(
+                        'Template context "%s"? ' % e.message))
+        call('osacompile -x -o %s %s' % (dst, tmp))
+    else:
         call('osacompile -x -o %s %s' % (dst, f))
 
 
@@ -47,7 +78,7 @@ def setup_parser():
         default=os.path.join(HOME, '.scripts'),
         help='directory where the scripts should be installed')
     parser.add_argument(
-        'admin-password', metavar='ADMIN_PASSWORD',
+        'admin_password', metavar='ADMIN_PASSWORD',
         help='admin password used for sudo functions')
     return parser
 
