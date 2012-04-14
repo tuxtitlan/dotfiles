95137252898e4c1e4d055279ce087c060822249f
George Song <george@55minutes.com>
2012-04-14 15:47:34 -0700

    Brew installs as a command

---
+++ bin/brew-install.sh
@@ -0,0 +1,23 @@
+brew update && brew upgrade && brew cleanup
+
+brew tap homebrew/versions
+
+brew install argyll
+brew install bash-completion
+brew install git
+brew install hub
+brew install macvim
+brew install mercurial --hard-code-python
+brew install multimarkdown
+brew install mysql
+brew install postgresql
+brew install python
+brew install python25
+brew install python26
+brew install qt
+brew install s3cmd
+brew install sleepwatcher
+brew install tree
+brew install wget
+
+brew unlink python && brew link python
