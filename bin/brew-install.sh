fbf99c8a3cb10eebb683d0943003c27acf0d9d27
George Song <george@damacy.net>
2013-06-09 16:20:30 -0700

    Updated home-brew installs

--- bin/brew-install.sh
+++ bin/brew-install.sh
@@ -1,11 +1,14 @@
 brew update && brew upgrade && brew cleanup
 
+brew tap homebrew/completions
 brew tap homebrew/dupes
 brew tap homebrew/versions
 
 brew install argyll
 brew install bash-completion
 brew install ctags-exuberant
+brew install curl-ca-bundle
+brew install django-completion
 brew install dos2unix --enable-nls
 brew install git
 brew install graphviz
@@ -13,9 +16,10 @@ brew install grep
 brew install hub
 brew install libyaml
 brew install macvim
-brew install mercurial --hard-code-python
+brew install mercurial
 brew install multimarkdown
 brew install mysql
+brew install node
 brew install postgresql
 brew install python
 brew install python25
@@ -25,6 +29,7 @@ brew install s3cmd
 brew install sleepwatcher
 brew install ssh-copy-id
 brew install swig
+brew install terminal-notifier
 brew install tree
 brew install wget
 
