4ee487455cb31d420421577c3461ec4fcebc20aa
George Song <george@damacy.net>
2018-04-18 14:16:24 -0700

    Start splitting out our bashrc scripts

---
+++ bashrc.d/01-history.bash
@@ -0,0 +1,3 @@
+export HISTIGNORE="&:ls:l:la:ll:exit"
+bind '"\e[A"':history-search-backward # up arrow
+bind '"\e[B"':history-search-forward  # down arrow
--- bashrc.d/20-completion.bash
+++ bashrc.d/20-completion.bash
@@ -1,16 +1,3 @@
-###########################
-# History and Completions #
-
-export HISTIGNORE="&:ls:l:la:ll:exit"
-bind '"\e[A"':history-search-backward # up arrow
-bind '"\e[B"':history-search-forward  # down arrow
-
+# shellcheck source=/dev/null
 [[ -f $(brew --prefix)/share/bash-completion/bash_completion ]] && \
-  source $(brew --prefix)/share/bash-completion/bash_completion
-
-complete -W "$(teamocil --list)" teamocil
-
- LUNCHY_DIR=$(dirname `gem which lunchy`)/../extras
- if [ -f $LUNCHY_DIR/lunchy-completion.bash ]; then
-   . $LUNCHY_DIR/lunchy-completion.bash
- fi
+  source "$(brew --prefix)"/share/bash-completion/bash_completion
