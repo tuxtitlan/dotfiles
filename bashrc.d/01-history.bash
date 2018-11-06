13f548cf93548a1b87deae95e7a1e94f3243878f
George Song <george@damacy.net>
2018-11-06 07:22:11 -0800

    Adjust bash history settings

--- bashrc.d/01-history.bash
+++ bashrc.d/01-history.bash
@@ -1,3 +1,7 @@
-export HISTIGNORE="&:ls:l:la:ll:exit"
+export HISTCONTROL=ignoredups:erasedups
+export HISTFILESIZE=10000
+export HISTIGNORE="&:?:??:exit:history:[j ]*:tmux"
+export HISTSIZE=1000
+shopt -s histappend
 bind '"\e[A"':history-search-backward # up arrow
 bind '"\e[B"':history-search-forward  # down arrow
