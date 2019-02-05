fe3e81afa50d4637973487171da1e82567c4f3ad
George Song <george@damacy.net>
2019-02-04 16:13:04 -0800

    Various updates

--- bashrc.d/10-node.bash
+++ bashrc.d/10-node.bash
@@ -1,4 +1,4 @@
-NODE_PATH=/usr/local/opt/node@10/bin
-[[ -d $NODE_PATH ]] && export PATH=$PATH:$NODE_PATH
+NODE_PATH_=/usr/local/opt/node@10/bin
+[[ -d $NODE_PATH_ ]] && export PATH=$PATH:$NODE_PATH_
 hash npx 2>/dev/null && source <(npx --shell-auto-fallback bash)
 hash npm 2>/dev/null && source <(npm completion)
